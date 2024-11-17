b1393c95d7c374f16ef49ed6532d966569d6bcb8
Octav02 <octavian.cosofret02@gmail.com>
2024-11-17 21:12:06 +0200

    Finalised all not naming

--- my-ionic-app/src/todo/MoviesList.tsx
+++ my-ionic-app/src/todo/MoviesList.tsx
@@ -27,6 +27,10 @@ import { NetworkState } from '../pages/NetworkState';
 import { Movie } from './Movie';
 import './MovieList.css'; // Import the CSS file
 
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
 const log = getLogger('MoviesList');
 const moviesPerPage = 5;
 const filterValues = ["IsElectric", "IsNotElectric"];
@@ -103,6 +107,14 @@ export const MoviesList: React.FC<RouteComponentProps> = ({ history }) => {
     await fetchData();
     await ($event.target as HTMLIonInfiniteScrollElement).complete();
   }
+  const icon = new L.Icon({
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    shadowSize: [41, 41]
+  });
 
   return (
     <IonPage>
@@ -159,6 +171,28 @@ export const MoviesList: React.FC<RouteComponentProps> = ({ history }) => {
             ))}
           </IonList>
         )}
+
+<MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '400px', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {moviesAux && moviesAux.map((movie) => (
+        <Marker
+          key={movie._id}
+          position={[movie.latitude || 42, movie.longitude || 42]}
+          icon={icon}
+        >
+          <Popup>
+            <div>
+              <h3>{movie.producer}</h3>
+              <p>{movie.model}</p>
+            </div>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+
         <IonInfiniteScroll
           threshold="100px"
           disabled={!more}
@@ -195,7 +229,9 @@ export const MoviesList: React.FC<RouteComponentProps> = ({ history }) => {
           onDidDismiss={closeShowSuccess}
           duration={5000}
         />
+      
       </IonContent>
+
     </IonPage>
   );
 
